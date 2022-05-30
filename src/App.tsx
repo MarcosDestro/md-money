e0b8d3ec62522acb0645cbde422945c465c788d0
MarcosDestro <marcosrdmoraes@gmail.com>
2022-05-30 19:05:47 -0300

    Add react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
+import Modal from 'react-modal';
+import { useState } from 'react';
+
+// Inidicando qual elemento deve estar o modal (Questão de acessibilidade)
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionsModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionsModalOpen(false);
+  }
   
   return (
     <>
-      <Header />
+      <Header 
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionsModalOpen} //Condição do modal
+        onRequestClose={handleCloseNewTransactionModal} // Oque executar se usuário fechar
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   )
