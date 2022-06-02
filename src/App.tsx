efab1abe602d883cd591ead2712c1ef3db9a7090
MarcosDestro <marcosrdmoraes@gmail.com>
2022-06-02 19:12:34 -0300

    Modal form styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
 import Modal from 'react-modal';
 import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 // Inidicando qual elemento deve estar o modal (Questão de acessibilidade)
 Modal.setAppElement("#root");
@@ -10,10 +11,13 @@ Modal.setAppElement("#root");
 export function App() {
   const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] = useState(false);
 
+  /** As funções e o estado não acompanham o componente, pois dificultaria
+   *  a passagem das funções e do estado para o componente botão, ou usar
+   *  a função de contexto, então é mais fácil usar o componente pai
+   */
   function handleOpenNewTransactionModal() {
     setIsNewTransactionsModalOpen(true);
   }
-
   function handleCloseNewTransactionModal() {
     setIsNewTransactionsModalOpen(false);
   }
@@ -24,12 +28,11 @@ export function App() {
         onOpenNewTransactionModal={handleOpenNewTransactionModal}
       />
       <Dashboard />
-      <Modal
-        isOpen={isNewTransactionsModalOpen} //Condição do modal
-        onRequestClose={handleCloseNewTransactionModal} // Oque executar se usuário fechar
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionsModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}/>
+      
       <GlobalStyle />
     </>
   )
