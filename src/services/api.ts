a05dca799929cc443ab42bffdd88c81ce0a44969
MarcosDestro <marcosrdmoraes@gmail.com>
2022-06-05 10:47:42 -0300

    Production build with api address

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    // baseURL: "http://localhost:3000/api",
+    baseURL: "https://md-money.vercel.app/api",
 })
